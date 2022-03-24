9936c7b4663b2b5bbc342eaeb99d34fac3c65eeb
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-03-24 12:03:09 +0300

    add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,60 @@
+export interface Iposer {
+  url: string,
+  width: number,
+  height: number
+}
+
+export interface Ithumbnails {
+  default:Iposer,
+  medium: Iposer,
+  high: Iposer,
+  standard: Iposer,
+  maxres: Iposer
+}
+
+export interface Ilocalized{
+  title: string,
+  description: string
+}
+
+export interface Isnippet {
+  publishedAt: string,
+  channelId: string,
+  title: string,
+  description: string,
+  thumbnails: Ithumbnails,
+  channelTitle: string,
+  tags: string[],
+  categoryId: string,
+  liveBroadcastContent: string,
+  localized: Ilocalized,
+  defaultAudioLanguage: 'en-US' | 'en' | 'ru'
+}
+
+export interface Istatistics {
+  viewCount: string,
+  likeCount: string,
+  dislikeCount: string,
+  favoriteCount: string,
+  commentCount: string
+}
+
+export interface Ivideo{
+  kind: string,
+  etag: string,
+  id: string,
+  snippet: Isnippet,
+  statistics: Istatistics
+}
+
+export interface IpageInfo{
+  totalResults: number,
+  resultsPerPage: number
+}
+
+export interface Iresponce {
+  kind: string,
+  etag: string,
+  pageInfo: IpageInfo,
+  items: Ivideo[]
+}
