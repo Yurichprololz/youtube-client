96e0f6a535958c31fde4221db75fc29deae982c5
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-03-28 17:30:38 +0300

    feat: add toggle sort buttons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @Output() toggleSort: EventEmitter<any> = new EventEmitter<any>();
+
+  toggle(){
+    this.toggleSort.emit();
+  }
+
 }
