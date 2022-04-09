3b3aac6f517a438b639f7e80093c00cfdf01a732
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-04-09 08:01:01 +0300

    refactor: delete coments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
-import { /* Router, */ RouterModule, Routes } from '@angular/router';
-// import { LoginGuardGuard } from './core/guards/login-guard.guard';
+import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
-// import { MainPageComponent } from './youtube/pages/main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main',  pathMatch:'full' },
--- src/app/core/guards/login-guard.guard.ts
+++ src/app/core/guards/login-guard.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { /* ActivatedRouteSnapshot, RouterStateSnapshot, */ CanActivate, Router,  UrlTree } from '@angular/router';
+import { CanActivate, Router,  UrlTree } from '@angular/router';
 import { LoginService } from '@src/app/auth/services/login.service';
 import { Observable } from 'rxjs';
 
@@ -9,8 +9,7 @@ import { Observable } from 'rxjs';
 export class LoginGuardGuard implements CanActivate {
   constructor(private router :Router, private loginService :LoginService){}
 
-  canActivate(/* route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot */): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.loginService.isLogin() || this.router.createUrlTree(['/login']);
   }
 
